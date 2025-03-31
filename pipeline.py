729624f571069e0ef7250ffab67045556fbf93b6
arkiver <arkiver@protonmail.com>
2025-03-31 06:51:52 +0200

    Version 20250331.01. Update user agent.

--- pipeline.py
+++ pipeline.py
@@ -77,8 +77,8 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250330.05'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
+VERSION = '20250331.01'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
 TRACKER_ID = 'sonetblog'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 100
